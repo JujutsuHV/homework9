c375ced44b5b04aa7356f123ec541d9c261be889
Veronika Postnikova <heyverkim@gmail.com>
2023-08-09 21:37:03 +0600

    done homework

---
+++ index.js
@@ -0,0 +1,44 @@
+let num = 40000;
+let num2 = 22000;
+console.log(num + num2);
+
+let city = 'Владивосток';
+let num3 = 2000;
+console.log(city + " " + num3);
+
+let messanger = 128;
+let likePhoto = 254;
+let reading = 137;
+let watching = 201;
+console.log(messanger + likePhoto + reading + watching);
+
+let x = 2;
+let y = 5;
+console.log(x ** y);
+
+let message = "Нужно бежать со всех ног, чтобы только оставаться на месте, а чтобы куда-то попасть, надо бежать как минимум вдвое быстрее!"
+console.log(message);
+
+const greetings = "Привет";
+const name = "Кот";
+console.log(`${greetings}, ${name}`)
+
+
+let time;
+time = 34;
+console.log(time)
+console.log('Старт поездки. Осталось минут:' + time);
+
+time = 15;
+console.log('Немного сториз в соцсетях. Осталось минут: ' + time);
+
+time = 10;
+console.log('Немного не новостей, но событий. Осталось минут: ' + time);
+
+time = 0;
+console.log('Вы приехали. Добро пожаловать в Москву');
+
+let fahrenheit = `451`;
+console.log((fahrenheit - 32) / 1.8);
+let celsius = `232.78`;
+console.log(`${fahrenheit} градуса по Фаренгейту - это ${celsius} градуса по Цельсию.`);
